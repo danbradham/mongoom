7467dd74c7a5b91761cae8b52403ea0a7d587d6f
Dan Bradham <dan@brandnewschool.com>
2014-02-12 16:51:02 -0500

    Event docs

--- mongoom/events.py
+++ mongoom/events.py
@@ -3,17 +3,24 @@ from .documents import Document
 from .fields import Field
 
 
-def fire(event, *args, **kwargs):
+def fire(event, **data):
     '''A convenience function for firing an event. "Firing an event" is
     exactly the same as saving an Event document to the database.
 
-    :param *args: Event documents __init__ args
-    :param **kwargs:: Event documents __init__ kwargs
+    :param **data: Event documents data. Same signature as the Event class.
     '''
-    event(*args, **kwargs).save()
+    event(**data).save()
 
 
 class Event(Document):
+    '''Event documents live in a capped mongodb collection. Allowing
+    people to Subscribe to events using a tailable cursor. Inherit from
+    Event to create custom events.
+
+    :param ref: Document that the event refers to.
+    :param user: User that fired the event.
+    :param created: Date that the event was fired.
+    '''
     _collection = {"name": "Event", "capped": True, "max": 100, "size": 2**20}
     ref = Field(Document)
     user = Field(Document)
