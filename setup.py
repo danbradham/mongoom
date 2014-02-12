000f12874e85a926d5881fe924b8534008ea0b0d
Dan Bradham <dan@brandnewschool.com>
2014-02-12 14:50:16 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ try:
     from distutils.core import setup
 except ImportError:
     from setuptools import setup
-from pymongorm import __version__
+from mongoom import __version__
 
-with open("Readme.md") as f:
+with open("Readme.rst") as f:
     readme = f.read()
 
 with open("LICENSE") as f:
@@ -13,15 +13,15 @@ with open("LICENSE") as f:
 package_data = {"": ["LICENSE"]}
 
 setup(
-    name="PyMongORM",
+    name="Mongoom",
     version=__version__,
-    description="An Object-Relational Mapping for MongoDB.",
+    description="An Object-Mapper for MongoDB.",
     long_description=readme,
     author="Dan Bradham",
     author_email="danielbradham@gmail.com",
     url="http://www.danbradham.com",
-    packages=["pymongorm"],
-    package_dir={"pymongorm": "pymongorm"},
+    packages=["mongoom"],
+    package_dir={"mongoom": "mongoom"},
     package_data=package_data,
     include_package_data=True,
     license=license,
